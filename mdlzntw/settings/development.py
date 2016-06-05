81e8a7853ab244695aaaf120cafbbd4092084fd3
Phil Harm <phillip.harm@gmail.com>
2016-06-05 09:11:16 -0400

    Delete development.py

--- mdlzntw/settings/development.py
+++
@@ -1,6 +0,0 @@
-from .base import *
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-#Future Phil: run --insecure on manage.py if you want to false above, otherwise hosting service will load static files
